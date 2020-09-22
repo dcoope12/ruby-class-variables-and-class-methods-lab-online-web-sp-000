3eff3619a9d4c29e3356d7f29f62afa803464ef6
Devin Cooper <dcoope12@asu.edu>
2020-09-22 17:35:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,5 +22,32 @@ def self.genres
   @@genres.uniq!
 end
 
+def self.artists
+@@artists.uniq!
+end
+
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] #if the genre is already in the hash
+        genre_count[genre] += 1
+      else #if it's a new genre
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] 
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
 end
